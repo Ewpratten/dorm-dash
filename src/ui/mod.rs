822fd6913acfe1407a20f35d1555ee087b07b1e9
Evan Pratten <ewpratten@gmail.com>
2021-09-24 14:29:39 -0400

    Add welcome message

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,13 +3,20 @@ use std::io::Stdout;
 use termion::{event::Key, raw::RawTerminal, screen::AlternateScreen};
 use tui::{
     backend::TermionBackend,
-    layout::{Constraint, Direction, Layout},
+    layout::{Alignment, Constraint, Direction, Layout},
+    text::{Span, Text},
+    widgets::{Block, Paragraph},
     Terminal,
 };
 
 use crate::data::DataFetchService;
 
-use self::components::{clock::build_clock, log::build_log_viewer, notification_feed::build_notification_feed, weather::{build_weather_info, render_weather_graph}};
+use self::components::{
+    clock::build_clock,
+    log::build_log_viewer,
+    notification_feed::build_notification_feed,
+    weather::{build_weather_info, render_weather_graph},
+};
 use self::tui_event::{Event, Events};
 
 mod components;
@@ -69,18 +76,31 @@ pub fn block_render_looping(
                 // Set up center panel
                 let center_panel = Layout::default()
                     .direction(Direction::Vertical)
-                    .constraints(
-                        [
-                            Constraint::Percentage(50),
-                            Constraint::Percentage(50),
-                        ]
-                        .as_ref(),
-                    )
+                    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
                     .split(chunks[1]);
 
                 // Render the log viewer
                 let log_viewer = build_log_viewer(&data_frame);
                 f.render_widget(log_viewer, center_panel[1]);
+
+                // Render a welcome message
+                let welcome_layout = Layout::default()
+                    .direction(Direction::Vertical)
+                    .constraints(
+                        [
+                            Constraint::Ratio(1, 3),
+                            Constraint::Ratio(1, 3),
+                            Constraint::Ratio(1, 3),
+                        ]
+                        .as_ref(),
+                    )
+                    .split(center_panel[0]);
+                let welcome_message = Paragraph::new(Text::raw(
+                    "Welcome to Evan's room!\nThis display shows real-time updates for the area\n\nSee: https://github.com/ewpratten/dorm-dash",
+                ))
+                .alignment(Alignment::Center)
+                .block(Block::default());
+                f.render_widget(welcome_message, welcome_layout[1]);
             })
             .unwrap();
 
